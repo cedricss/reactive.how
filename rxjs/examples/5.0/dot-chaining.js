09c3416ff3fe3d8ce417e2c7f337b2b6cc9ecaed
Cédric Soulas <soulascedric@gmail.com>
2018-04-11 10:20:28 +0200

    Update RxJS Explorer 5.0 dot-chaining example

--- rxjs/examples/5.0/dot-chaining.js
+++ rxjs/examples/5.0/dot-chaining.js
@@ -1,12 +1,17 @@
-import { Observable } from "rxjs/Observable"
-import "rxjs/add/observable/interval"
-import "rxjs/add/operator/map"
-import "rxjs/add/operator/take"
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/interval";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/take";
 
-import { TICK, LENGTH, gaussian } from "./base"
+import { TICK, LENGTH, gaussian } from "./base";
 
+/**
+ * The legacy dot-chaining operators.
+ * You should now use pipeable operators instead.
+ */
 const stream = Observable.interval(TICK)
-  .take(LENGTH)
   .map(gaussian)
+  .map(num => "•".repeat(Math.floor(num * 65)))
+  .take(LENGTH);
 
-stream.subscribe(console.log)
+stream.subscribe(console.log);
